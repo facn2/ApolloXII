74e21adb6adc5d2f46e40ca529c0783fb88afcb1
rinoma <ganzo.s@libero.it>
2017-07-26 16:03:55 +0300

    search function Relates #18

--- src/search.js
+++ src/search.js
@@ -1,5 +1,28 @@
+const food = require(`./food.json`);
+
 const searchfunc = (partialSearch) => {
-  console.log("we made it : " + partialSearch);
+  console.log("We are looking for: " + partialSearch);
+  var resultObj = [];
+  var counter = 0;
+  food.forEach(function(obj) {
+    let name = obj.nm.toLowerCase();
+    if ((name.indexOf(partialSearch.toLowerCase()) === 0) && counter < 10 ) {
+
+      resultObj.push(JSON.stringify(name));
+      counter ++;
+
+
+    }
+    //  else if (name.indexOf(partialSearch.toLowerCase()) !== -1) {
+    //   console.log(name);
+    //}
+    // else {
+    //   console.log("Sorry NOTHING!")
+    // }
+  });
+  console.log(resultObj);
 }
+
+
 
 module.exports = {searchfunc};
