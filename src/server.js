1556ea5a1d3eb14c6ee2668d98b1b2a6fa1e2048
rinoma <ganzo.s@libero.it>
2017-07-27 11:38:08 +0300

    exporting server.js function removed Relates #42

--- src/server.js
+++ src/server.js
@@ -8,12 +8,7 @@ const port = process.env.PORT || 4000;
 const router = require('./router.js');
 
 const server = http.createServer(router);
-// console.log(server);
 
 server.listen(port);
 
 console.log('server running on: http://' + host + ':' + port);
-
-module.exports = {
-  server: server,
-}
