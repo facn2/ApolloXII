0687c0c563cf83cfc499c762b06382aff37e8e83
rinoma <ganzo.s@libero.it>
2017-07-27 12:31:47 +0300

    improved sintax for reouter and search js Relates #57

--- src/router.js
+++ src/router.js
@@ -8,17 +8,11 @@ const router = (request, response) => {
   } else if (url.indexOf(`/public/`) === 0) {
     handlers.handlePublic(response, url)
   } else if (url.indexOf(`&submit=`) !== -1) {
-
-    const finalSearchSubmit = request.url.split(`search=`)[1];
-    const finalSearch = finalSearchSubmit.split(`&submit`)[0];
-    console.log("Search:"+finalSearch);
-    var finalResult = JSON.stringify(search.finalSearchFunc(finalSearch));
+    var finalResult = search.finalSearchFunc(request);
     response.writeHead(200, {"Location": "../"});
     response.end(`<h1>You looked for: </h1>` + finalResult)
   } else if (url.indexOf(`/?search=`) === 0) {
-    var partialSearch = request.url.split(`search=`)[1];
-    var resultSearch = JSON.stringify(search.searchfunc(partialSearch));
-    response.end(resultSearch)
+    response.end(JSON.stringify(search.searchfunc(request)))
   }  else {
     response.writeHead(404)  //Header of HTTP request
     response.end(`<h1>404 File not found</h1>`)
